8fec3cd9a465d2d74c184b5dfbd2ece94ac63e13
ana <ana.bernardes@zup.com.br>
2023-04-27 16:57:13 -0300

    primeiro commit

---
+++ AulaDoze/src/DoWhileForWhile/DoWhile.java
@@ -0,0 +1,13 @@
+package DoWhileForWhile;
+
+public class DoWhile {
+    public static void main (String[] args){
+        int contar = 0;
+            do { ;System.out.println(contar);contar++;
+                contar++; }
+            while (contar <= 100);
+
+
+        }
+    }
+
---
+++ AulaDoze/src/DoWhileForWhile/TabuadaFor.java
@@ -0,0 +1,12 @@
+package DoWhileForWhile;
+
+public class TabuadaFor{
+    public static void main(String[] args) {
+        for (int i = 1; i <=10;i++) {
+            int resultado = 2 * i;
+            System.out.println("2 x" + i + "=" + resultado);
+        }
+}
+};
+
+
